94a2fefa0fcec1eb588d31b04656655029ac23ed
maoxiaomin <34832146+maoxiaomin@users.noreply.github.com>
2023-03-23 16:51:55 +0800

    Update checkincookie_env.js

--- checkincookie_env.js
+++ checkincookie_env.js
@@ -123,7 +123,6 @@ function checkin(url, cookie, name) {
     headers: { Cookie: cookie },
   };
   $.post(checkinrequest, (error, response, data) => {
-    $.msg(name, "签到失败", data);
     if (error) {
       console.log(error);
       $.msg(name, "签到失败", error);
